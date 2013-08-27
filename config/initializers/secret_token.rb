da3db133b2bd37fb9f98adf798be696b9cf632a1
Jimmy Liang <jimmy.liang@gmail.com>
2013-08-27 14:22:19 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wooul::Application.config.secret_key_base = '7dfe3811777363ceb0a5ae9c2f181dfd8763b9f1745db63f403fbf53b3d6cfba4e57c34eeeddf156f2821117b14e90822282aa35d2f99b8b992ecfc598b71681'
