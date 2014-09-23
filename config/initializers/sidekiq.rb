c371f411073df95b05962e4cc4f29b1910e33746
dominic <dominic0312@126.com>
2014-09-23 16:52:30 +0800

    no comment

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_client do |config|
+  config.redis = { :namespace => 'wul', :url => 'redis://127.0.0.1:6379/1' }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :namespace => 'wul', :url => 'redis://127.0.0.1:6379/1' }
+end
